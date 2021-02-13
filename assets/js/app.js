a93dd9626ba336ea8fd72a39c0b07dffb68d7013
LukeMcCann <developer-lukemccann@outlook.com>
2021-02-13 14:26:52 +0000

    Add logic for selecting buttons from loops

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,11 @@
-alert('hello');
+let drums = document.querySelectorAll(".drum");
+
+for (let i = 0; i < drums.length; i++) {
+    drums[i].addEventListener("click", function () {
+        handle(drums[i].innerHTML);
+    });
+}
+
+function handle(button) {
+    alert(`${button}: clicked`);
+}
