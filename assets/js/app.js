f9ffaade11614d7f0446c4d5d44358f53a357077
LukeMcCann <developer-lukemccann@outlook.com>
2021-02-13 15:46:11 +0000

    Add sounds on button clicks

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,46 @@ let drums = document.querySelectorAll(".drum");
 
 for (let i = 0; i < drums.length; i++) {
     drums[i].addEventListener("click", function () {
-        handle(drums[i].innerHTML);
+       handleButtonClick(this)
     });
 }
 
-function handle(button) {
-    alert(`${button}: clicked`);
+function handleButtonClick(button) {
+    if (button.textContent.includes("W")) {
+        play("tom-1");
+        return 0;
+    }
+
+    if (button.textContent.includes("A")) {
+        play("tom-2");
+        return 0;
+    }
+
+    if (button.textContent.includes("S")) {
+        play("tom-3");
+        return 0;
+    }
+
+    if (button.textContent.includes("D")) {
+        play("tom-4");
+        return 0;
+    }
+
+    if (button.textContent.includes("J")) {
+        play("snare");
+        return 0;
+    }
+
+    if (button.textContent.includes("K")) {
+        play("crash");
+    }
+
+    if (button.textContent.includes("L")) {
+        play("kick-bass");
+    }
+}
+
+function play(fileName) {
+    let audio = new Audio(`/assets/sounds/${fileName}.mp3`);
+    audio.play();
 }
