bf231912409efe0880ff5cdc95430af4b19521fc
LukeMcCann <developer-lukemccann@outlook.com>
2021-02-13 16:37:39 +0000

    add button animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,46 +2,48 @@ let drums = document.querySelectorAll(".drum");
 
 for (let i = 0; i < drums.length; i++) {
     drums[i].addEventListener("click", function () {
-       handleInputEvent(this.textContent.toUpperCase())
+       handleInputEvent(this.textContent.toLowerCase());
+       animateButton(this.textContent.toLowerCase());
     });
 }
 
 document.addEventListener('keydown', function (event) {
-    handleInputEvent(event.key.toUpperCase());
+    handleInputEvent(event.key.toLowerCase());
+    animateButton(event.key.toLowerCase());
 });
 
 function handleInputEvent(button) {
-    if (button.includes("W")) {
+    if (button.includes("w")) {
         play("tom-1");
         return 0;
     }
 
-    if (button.includes("A")) {
+    if (button.includes("a")) {
         play("tom-2");
         return 0;
     }
 
-    if (button.includes("S")) {
+    if (button.includes("s")) {
         play("tom-3");
         return 0;
     }
 
-    if (button.includes("D")) {
+    if (button.includes("d")) {
         play("tom-4");
         return 0;
     }
 
-    if (button.includes("J")) {
+    if (button.includes("j")) {
         play("snare");
         return 0;
     }
 
-    if (button.includes("K")) {
+    if (button.includes("k")) {
         play("crash");
         return 0;
     }
 
-    if (button.includes("L")) {
+    if (button.includes("l")) {
         play("kick-bass");
         return 0;
     }
@@ -53,4 +55,13 @@ function handleInputEvent(button) {
 function play(fileName) {
     let audio = new Audio(`/assets/sounds/${fileName}.mp3`);
     audio.play();
+}
+
+function animateButton(button) {
+    let activeButton = document.querySelector(`.${button}`);
+    activeButton.classList.add('pressed');
+
+    setTimeout(function () {
+        activeButton.classList.remove('pressed');
+    }, 100);
 }
